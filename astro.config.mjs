86aa4222fe40e35f7c76696f3c6a3c65ee347ea6
Pablito0403 <pa0403.pablo@gmail.com>
2025-12-01 12:33:49 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  	i18n: {
+    	locales: ['es', 'pt', 'en'],
+    	defaultLocale: 'es',
+    	routing: {
+      		prefixDefaultLocale: true
+    	}
+  	},
+  	vite: {
+  	  	plugins: [tailwindcss()]
+  	}
+});
